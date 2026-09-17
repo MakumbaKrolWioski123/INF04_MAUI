c652af255da274a4ba2f5974779098c2b0f347c3
MakumbaKrolWioski123 <ut_4010@zsp3zamosc.pl>
2026-09-17 11:33:14 +0200

    Zadanie1.3

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Przycisk_Powitanie_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            Etykieta_Powitania.Text = "Dziekujemy za uruchomienie aplikacji.";
+            Licznik.Text = $"Licznik kliknieć: {count}";
+        }
+
+        private void Przycisk_Reset_Clicked_1(object sender, EventArgs e)
+        {
+            count++;
+            Etykieta_Powitania.Text = "Witamy w aplikacji!";
+            Licznik.Text = $"Licznik kliknieć: {count}";
+        }
+
+
+    }
+}
